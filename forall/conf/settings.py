e1b0f180f1340c50e40432833576ed8d6c5b20e1
Misael <misaelrh@proton.me>
2026-05-27 15:29:33 -0300

    chore: começa a organizar os arquivos do projeto, consertando o gitignore removendo pycache

--- forall/conf/settings.py
+++ forall/conf/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    #'user',
     #'post'
 ]
 
@@ -105,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Pra funcionar os usuários
+AUTH_USER_MODEL = 'profiles.Profile'
+LOGIN_URL = 'profile:login'
